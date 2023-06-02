b8b55d1526f64c3914f72efb06bbee340b654fab
Luwam Abraham <luwam.yihdego@miu.edu>
2023-06-02 14:10:05 -0500

    Home Page
    Hope page

---
+++ redsee/src/components/Body.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Body.css";
+import airplaneImage from "./../assets/images/airoplain.png";
+
+const Body = () => {
+  return (
+    <div className="body-container">
+      <h1 className="body-heading">Welcome to Eritrean Airline</h1>
+      <p className="body-paragraph">
+        Eritrean Airlines is a government-financed endeavor established in May
+        1991, when independence was won. Long time it remained an airline in
+        name only, serving as the only ground handling agent at Asmara
+        International Airport and at Assab and Massawa. The airline has also
+        served as a sales agent for other major carriers which fly to Eritrea.
+        The expansion of the company’s operations to flights was decided at a
+        workshop held in Asmara in May 2002. In April 2003 Eritrean Airlines
+        took delivery of its first 767-300ER (extended range) airplane as
+        preparations for new operations to begin. The airline's first Boeing
+        767-300ER, “Queen Bee” is being leased (with the option of purchase) as
+        the first of a series of airplanes that will enter the carrier’s fleet.
+        The 767 is Boeing’s most popular wide-body airplane and has a
+        well-earned reputation (more than 900 airplanes of this type with more
+        than 80 operators around the world). Eritrean Airlines, short name B8
+        and member of IATA, operates scheduled passenger and cargo services from
+        its Eritrean headquarters in Asmara to Rome, Cairo, Jeddah, Dubai
+        andKhartoum, with more destinations to be added in the near future.
+        Relief goods donated by the governments and relief organizations of
+        several EU countries is the main cargo flying to Asmara, fresh exclusive
+        fishery products travel in the opposite direction. Many of the pilots,
+        mechanics and inspectors of Eritrean Airlines have their roots and
+        received their training in Ethiopia as employees of Ethiopian Airlines,
+        but were expelled from Ethiopia by the TPLF regime. The flights
+        attendants were recruited from the Asmara Intercontinental Hotel.
+      </p>
+      <img src={airplaneImage} alt="Airplane" className="airplane-image" />
+    </div>
+  );
+};
+
+export default Body;
---
+++ redsee/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__content">
+          <div className="footer__section">
+            <h3>Contact Us</h3>
+            <p>Email: info@example.com</p>
+            <p>Phone: 123-456-7890</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
